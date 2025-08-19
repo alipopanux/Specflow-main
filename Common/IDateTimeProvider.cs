a6c8238cbac064221407228c8784efe200e56911
alipopanux <157824000+alipopanux@users.noreply.github.com>
2025-08-19 12:10:27 +0200

    Delete Common directory

--- Common/IDateTimeProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lopcommerce.Regles.WebAPI.Tests.Common
-{
-    public class IDateTimeProvider
-    {
-        public DateTime Now => new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
-        public DateTimeOffset NowOffset => new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
-    }
-}
