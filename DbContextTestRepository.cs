b9f250dc8fb53671c0d2d19ac1a449b999a34a92
alipopanux <157824000+alipopanux@users.noreply.github.com>
2025-08-19 12:11:17 +0200

    Delete DbContextTestRepository.cs

--- DbContextTestRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lopcommerce.Regles.DataAccess.Entities;
-
-namespace Lopcommerce.Regles.WebAPI.Tests
-{
-    public class DbContextTestRepository(ConstanteDBContext dbContext) : IDbTestRepository
-    {
-        private readonly ConstanteDBContext _dbContext = dbContext;
-    }
-}
